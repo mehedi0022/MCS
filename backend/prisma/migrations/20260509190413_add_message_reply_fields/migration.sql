bcceccebb876a9434c80fa32236f7f3c5ff29cf1
MD Mehedi Hassan <mdmehedihassan0022@gmail.com>
2026-05-12 03:46:31 +0600

    Update Migration

--- backend/prisma/migrations/20260509190413_add_message_reply_fields/migration.sql
+++ backend/prisma/migrations/20260509190413_add_message_reply_fields/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
-ALTER TABLE `contactmessage` ADD COLUMN `repliedAt` DATETIME(3) NULL,
+ALTER TABLE `ContactMessage` ADD COLUMN `repliedAt` DATETIME(3) NULL,
     ADD COLUMN `repliedBy` VARCHAR(191) NULL,
     ADD COLUMN `replyMessage` TEXT NULL;
